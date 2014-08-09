5cfcbd0f708f118d95d14e415e53143f4090d03a
songxh <tsingheng@163.com>
2014-08-09 23:40:17 +0800

    加个登录页面

--- weixin-manage/src/main/java/net/shangtech/weixin/commons/controller/MainController.java
+++ weixin-manage/src/main/java/net/shangtech/weixin/commons/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class MainController {
 	
-	@RequestMapping("/main")
+	@RequestMapping("main")
 	public String main(){
 		return "main";
 	}
--- weixin-manage/src/main/java/net/shangtech/weixin/security/JdbcAuthenticationProvider.java
+++ weixin-manage/src/main/java/net/shangtech/weixin/security/JdbcAuthenticationProvider.java
@@ -1,27 +1,22 @@
 package net.shangtech.weixin.security;
 
-import net.shangtech.weixin.commons.entity.SysUser;
-import net.shangtech.weixin.commons.service.ISysUserService;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetails;
 
 public class JdbcAuthenticationProvider implements AuthenticationProvider {
 
-	@Autowired
-	private ISysUserService service;
+//	@Autowired
+//	private ISysUserService service;
 	
 	public Authentication authenticate(Authentication authentication)
 			throws AuthenticationException {
-		UserDetails details = (UserDetails) authentication.getPrincipal();
-		String username = details.getUsername();
-		String password = details.getPassword();
-		SysUser user = service.findByUsername(username);
-		
-		return null;
+//		UserDetails details = (UserDetails) authentication.getPrincipal();
+//		String username = details.getUsername();
+//		String password = details.getPassword();
+//		SysUser user = service.findByUsername(username);
+//		
+		return authentication;
 	}
 
 	public boolean supports(Class<?> clazz) {
